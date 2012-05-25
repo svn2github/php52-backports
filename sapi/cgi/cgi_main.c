4ddaca36b09a869ad074c613e71e28ad2c24db3a
svyatoslav.lempert@gmail.com <svyatoslav.lempert@gmail.com@d3730abf-dca4-539c-c9c3-7a50776e413f>
2012-05-25 06:26:01 +0000

    CVE-2012-2311 fix
    git-svn-id: https://php52-backports.googlecode.com/svn/trunk@34 d3730abf-dca4-539c-c9c3-7a50776e413f

--- sapi/cgi/cgi_main.c
+++ sapi/cgi/cgi_main.c
@@ -1356,6 +1356,9 @@ int main(int argc, char *argv[])
 	int status = 0;
 #endif
 #endif /* PHP_FASTCGI */
+	char *query_string;
+	char *decoded_query_string;
+	int skip_getopt = 0;
 
 #if 0 && defined(PHP_DEBUG)
 	/* IIS is always making things more difficult.  This allows
@@ -1410,14 +1413,15 @@ int main(int argc, char *argv[])
 	}
 #endif
 
-	char *query_string;
-	char *decoded_query_string;
-	int skip_getopt = 0;
-
-	if(query_string = getenv("QUERY_STRING")) {
+	if((query_string = getenv("QUERY_STRING")) != NULL && strchr(query_string, '=') == NULL) {
+		/* we've got query string that has no = - apache CGI will pass it to command line */
+		unsigned char *p;
 		decoded_query_string = strdup(query_string);
 		php_url_decode(decoded_query_string, strlen(decoded_query_string));
-		if(*decoded_query_string == '-' && strchr(decoded_query_string, '=') == NULL) {
+		for (p = decoded_query_string; *p &&  *p <= ' '; p++) {
+			/* skip all leading spaces */
+		}
+		if(*p == '-') {
 			skip_getopt = 1;
 		}
 		free(decoded_query_string);
@@ -1677,7 +1681,7 @@ consult the installation file that came with this distribution, or visit \n\
 #endif /* FASTCGI */
 
 	zend_first_try {
-		while ((c = php_getopt(argc, argv, OPTIONS, &php_optarg, &php_optind, 1)) != -1) {
+		while (!skip_getopt && (c = php_getopt(argc, argv, OPTIONS, &php_optarg, &php_optind, 1)) != -1) {
 			switch (c) {
 #if PHP_FASTCGI
 				case 'T':
@@ -1802,10 +1806,6 @@ consult the installation file that came with this distribution, or visit \n\
 						behavior=PHP_MODE_INDENT;
 						break;
 #endif
-	char *query_string;
-	char *decoded_query_string;
-	int skip_getopt = 0;
-
   				case 'q': /* do not generate HTTP headers */
 						no_headers = 1;
 						break;
